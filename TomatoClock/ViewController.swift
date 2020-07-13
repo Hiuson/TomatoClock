c7cf33819e836edbc4598be806a48aefbf6ea0e6
zhouchun <zhouchun@kuaishou.com>
2020-07-13 10:47:30 +0800

    Initial Commit

---
+++ TomatoClock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TomatoClock
+//
+//  Created by zhouchun on 2020/7/13.
+//  Copyright © 2020 Hiuson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
